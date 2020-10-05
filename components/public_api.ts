191c45478899175a4d21fb7374613003aeac7e32
Dariusz <darekf77@gmail.com>
2020-10-05 23:29:15 +0200

    before backend stub from frontend lib

--- components/public_api.ts
+++ components/public_api.ts
@@ -1,3 +1,4 @@
 export { ButtonIconComponent, ButtonIconModule } from './button-icon';
 export { MoveablePopupComponent, MoveablePopupModule } from './moveable-popup';
 export { ListWrapperComponent, ListWrapperModule } from './list';
+export { FormWrapperMaterialComponent, FormWrapperMaterialModule } from './form';
