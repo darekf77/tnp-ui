1c08b454187dc7dcfe4a70d439c48a684e4bc621
Dariusz <darekf77@gmail.com>
2020-10-11 23:43:57 +0200

    progressing with draggable popup

--- components/draggable-popup/draggable-popup.component.ts
+++ components/draggable-popup/draggable-popup.component.ts
@@ -1,15 +1,89 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewContainerRef, Input, TemplateRef, Inject, ComponentFactoryResolver } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-draggable-popup',
-  templateUrl: './draggable-popup.component.html',
-  styleUrls: ['./draggable-popup.component.css']
+  templateUrl: 'draggable-popup.component.html',
+  styleUrls: ['draggable-popup.component.css']
 })
 export class DraggablePopupComponent implements OnInit {
+  @ViewChild('popupRoot', { read: ViewContainerRef }) parent: ViewContainerRef;
 
-  constructor() { }
+  @Input() public popupContent: TemplateRef<any>;
+  public dialogRef: MatDialogRef<DraggablePopupWindowComponent>;
+
+  constructor(
+    public dialog: MatDialog) {
+  }
+
+  trigger() {
+    this.dialogRef = this.dialog.open(DraggablePopupWindowComponent, {
+      width: '250px',
+      hasBackdrop: false,
+      disableClose: true,
+      data: this
+    });
+
+    this.dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
+  async ngOnInit() {
+
+  }
+
+}
+
+
+@Component({
+  selector: 'app-draggable-popup-window',
+  template: `
+
+  <h1 mat-dialog-title cdkDrag cdkDragRootElement=".cdk-overlay-pane" cdkDragHandle>Hi</h1>
+<div mat-dialog-content >
+  <p>What's your favorite animal?</p>
+
+  <div #container ></div>
+</div>
+<div mat-dialog-actions>
+  <button mat-button (click)="onNoClick()">No Thanks</button>
+  <button mat-button cdkFocusInitial>Ok</button>
+</div>
+
+
+  `,
+  styles: [``],
+})
+
+export class DraggablePopupWindowComponent implements OnInit {
+  @ViewChild('container', { read: ViewContainerRef }) view;
+  constructor(public dialogRef: MatDialogRef<DraggablePopupComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DraggablePopupComponent,
+  ) { }
 
   ngOnInit() {
+    console.log(this.data);
+  }
+
+  onNoClick() {
+
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+
+      this.view.createEmbeddedView(this.data.popupContent,
+        //   , {
+        //   model: this.model,
+        //   dialog: {
+        //     close: () => this.onClose()
+        //   }
+        // }
+      );
+
+    });
+
   }
 
 }
--- components/draggable-popup/draggable-popup.module.ts
+++ components/draggable-popup/draggable-popup.module.ts
@@ -1,11 +1,42 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DraggablePopupComponent } from './draggable-popup.component';
+import { A11yModule } from '@angular/cdk/a11y';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import { CdkTableModule } from '@angular/cdk/table';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { MatDialogModule } from '@angular/material/dialog';
+
+import { DraggablePopupComponent, DraggablePopupWindowComponent } from './draggable-popup.component';
+
+const angularModules = [
+  A11yModule,
+  ClipboardModule,
+  DragDropModule,
+  ScrollingModule,
+  CdkStepperModule,
+  CdkTableModule,
+  CdkTreeModule,
+  MatDialogModule,
+]
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ...angularModules,
   ],
-  declarations: [DraggablePopupComponent]
+  exports: [
+    DraggablePopupComponent,
+    ...angularModules,
+  ],
+  declarations: [
+    DraggablePopupComponent,
+    DraggablePopupWindowComponent,
+  ],
+  entryComponents: [
+    DraggablePopupWindowComponent,
+  ]
 })
 export class DraggablePopupModule { }
