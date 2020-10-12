18b2e7374fb073a28c5927378f62110b6eec6c7b
Dariusz Filipiak <dariusz.filipiak@igt.com>
2020-10-12 02:29:32 +0200

    pin in progress

--- components/draggable-popup/draggable-popup.component.ts
+++ components/draggable-popup/draggable-popup.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild, ViewContainerRef, Input, TemplateRef, Inject, ComponentFactoryResolver } from '@angular/core';
+import * as _ from 'lodash';
+import { Component, OnInit, ViewChild, ViewContainerRef, Input, TemplateRef, Inject, ComponentFactoryResolver, EventEmitter, Output } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
@@ -7,18 +8,33 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dial
   styleUrls: ['draggable-popup.component.css']
 })
 export class DraggablePopupComponent implements OnInit {
+  static popups: { [id: string]: DraggablePopupComponent } = {};
   @ViewChild('popupRoot', { read: ViewContainerRef }) parent: ViewContainerRef;
 
   @Input() public popupContent: TemplateRef<any>;
+  @Input() public id: string;
+  @Input() public title = '';
+  @Input() public pinned = false;
+  @Output() public close = new EventEmitter();
+  @Output() public pin = new EventEmitter();
   public dialogRef: MatDialogRef<DraggablePopupWindowComponent>;
 
   constructor(
     public dialog: MatDialog) {
   }
 
-  trigger() {
+  // reinitPos() { // TODO
+  //   const x = Number(localStorage.getItem(`${coordinateX}${this.id}`));
+  //   const y = Number(localStorage.getItem(`${coordinateY}${this.id}`));
+  //   if (!_.isNaN(x) && !_.isNaN(y)) {
+  //     this.moveTo(x, y);
+  //   }
+  // }
+
+  init() {
+    // @ts-ignore
     this.dialogRef = this.dialog.open(DraggablePopupWindowComponent, {
-      width: '250px',
+      width: '400px',
       hasBackdrop: false,
       disableClose: true,
       data: this
@@ -27,10 +43,22 @@ export class DraggablePopupComponent implements OnInit {
     this.dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
     });
+
   }
 
-  async ngOnInit() {
+  // public pin = (value) => {
+  //   if (this.onClose.observers.length > 0) {
+  //     this.onPin.next(value)
+  //   }
+  // }
 
+  async ngOnInit() {
+    if (!_.isNil(this.id) && !_.isObject(this.id)) {
+      DraggablePopupComponent.popups[this.id] = this;
+    }
+    if (this.pinned) {
+      this.init();
+    }
   }
 
 }
@@ -40,7 +68,11 @@ export class DraggablePopupComponent implements OnInit {
   selector: 'app-draggable-popup-window',
   template: `
 
-  <h1 mat-dialog-title cdkDrag cdkDragRootElement=".cdk-overlay-pane" cdkDragHandle>Hi</h1>
+  <h1 mat-dialog-title cdkDrag cdkDragRootElement=".cdk-overlay-pane" cdkDragHandle>
+  <span>{{parent.title}}</span>
+      <input type="checkbox" (change)="changePinned($event)"  [checked]="parent.pinned"  > pin &nbsp;&nbsp;&nbsp;
+      <label style="cursor:pointer;" (click)="closePopup()">X</label>
+  </h1>
 <div mat-dialog-content >
   <p>What's your favorite animal?</p>
 
@@ -59,31 +91,24 @@ export class DraggablePopupComponent implements OnInit {
 export class DraggablePopupWindowComponent implements OnInit {
   @ViewChild('container', { read: ViewContainerRef }) view;
   constructor(public dialogRef: MatDialogRef<DraggablePopupComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DraggablePopupComponent,
+    @Inject(MAT_DIALOG_DATA) public parent: DraggablePopupComponent,
   ) { }
 
-  ngOnInit() {
-    console.log(this.data);
+
+  ngOnInit() { }
+  onNoClick() { }
+  closePopup() {
+    this.dialogRef.close();
   }
-
-  onNoClick() {
-
+  changePinned(e) {
+    console.log('pinned', e.target.checked)
+    this.parent.pinned = e.target.checked;
   }
 
   ngAfterViewInit() {
     setTimeout(() => {
-
-      this.view.createEmbeddedView(this.data.popupContent,
-        //   , {
-        //   model: this.model,
-        //   dialog: {
-        //     close: () => this.onClose()
-        //   }
-        // }
-      );
-
+      this.view.createEmbeddedView(this.parent.popupContent);
     });
-
   }
 
 }
